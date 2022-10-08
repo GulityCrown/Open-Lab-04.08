7a8857712034180cc52a258634b20ab0e861c66f
GulityCrown <109987666+GulityCrown@users.noreply.github.com>
2022-10-08 20:25:06 +0200

    Update StringTools.cs

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +6,30 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    count++;
+                    
+                }
+
+            }
+            int k = 0;
+            string[] Letters = new string[count];
+            for (int j = 0; j < strings.Length; j++)
+            {
+                if (strings[j].Length == 4)
+                {
+                    Letters[k] = strings[j];
+                    k++;
+
+                }
+
+            }
+
+            return Letters;
         }
     }
 }
